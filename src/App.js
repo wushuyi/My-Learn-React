3fc9f13c3873b3870759ea70241fc243de7466fa
wushuyi <wushuyi541@gmail.com>
2017-03-02 14:15:44 +0800

    add ant and test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import {Button, Rate, DatePicker, Pagination} from 'antd';
+
+const {MonthPicker, RangePicker} = DatePicker;
+
+function onChange(date, dateString) {
+    console.log(date, dateString);
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='App'>
+                <Button type='primary'>Button</Button>
+                <div>
+                    <Rate />
+                </div>
+                <div>
+                    <DatePicker onChange={onChange}/>
+                    <br />
+                    <MonthPicker onChange={onChange} placeholder="Select month"/>
+                    <br />
+                    <RangePicker onChange={onChange}/>
+                </div>
+                <div>
+                    <Pagination defaultCurrent={6} total={500}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
